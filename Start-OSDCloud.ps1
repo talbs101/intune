e325c38d0062a8e9f550b78c506a243b70a61266
Talbs <159438310+talbs101@users.noreply.github.com>
2025-04-16 16:36:38 +0100

    Create Start-OSDCloud.ps1

---
+++ Start-OSDCloud.ps1
@@ -0,0 +1,26 @@
+Write-Output "--------------------------------------"
+Write-Output ""
+Write-Output "OSDCloud Apply OS Step"
+Write-Output ""
+#Set OSDCloud Params
+$OSName = "Windows 11 24H2 x64"
+Write-Output "OSName: $OSName"
+$OSEdition = "Pro"
+Write-Output "OSEdition: $OSEdition"
+$OSActivation = "Volume"
+Write-Output "OSActivation: $OSActivation"
+$OSLanguage = "en-gb"
+Write-Output "OSLanguage: $OSLanguage"
+
+
+
+
+
+#Launch OSDCloud
+Write-Output "Launching OSDCloud"
+Write-Output ""
+Write-Output "Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage"
+Write-Output ""
+Start-OSDCloud -ZTI -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage 
+Write-Output ""
+Write-Output "--------------------------------------"
