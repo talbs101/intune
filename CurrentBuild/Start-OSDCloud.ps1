39686fc0c3a83ca1bafc6fcd35c66f7fd878c59d
Talbs <159438310+talbs101@users.noreply.github.com>
2025-06-05 11:43:37 +0100

    Update Start-OSDCloud.ps1

--- CurrentBuild/Start-OSDCloud.ps1
+++ CurrentBuild/Start-OSDCloud.ps1
@@ -1,4 +1,14 @@
 #================================================
+#   [PreOS] Update Module
+#================================================
+
+#Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
+#Install-Module OSD -Force
+
+Write-Host  -ForegroundColor Green "Importing OSD PowerShell Module"
+Import-Module OSD -Force 
+
+#================================================
 #   [PreOS] Update Module & Environment Preparation
 #================================================
 
