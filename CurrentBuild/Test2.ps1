265a420664cf0b481910f5bc68dafda46de1bc7c
Talbs <159438310+talbs101@users.noreply.github.com>
2025-06-09 12:23:54 +0100

    Update Test2.ps1

--- CurrentBuild/Test2.ps1
+++ CurrentBuild/Test2.ps1
@@ -51,3 +51,52 @@ else {
 # Decrypting no matter the current status so that Intune policy enforces Encryption to 256bit strength
 Write-Host -ForegroundColor Green "Decrypting BitLocker"
 Manage-bde -off C: 
+
+#=======================================================================
+#   [OS] Enable Location Services
+#=======================================================================
+
+# Required to fix the issue where Intune doesn't obtain the WiFi mac address
+Write-Host -ForegroundColor Green "Enabling Location Services"
+
+# Define the registry path
+$registryPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\location"
+
+# Check if the registry key exists, if not, create it
+if (-not (Test-Path $registryPath)) {
+    New-Item -Path $registryPath -Force | Out-Null
+}
+
+# Set the registry value
+Set-ItemProperty -Path $registryPath -Name "Value" -Type String -Value "Allow" 
+
+#=======================================================================
+#   [OS] Install SimpleHelp
+#=======================================================================
+
+
+Write-Host -ForegroundColor Green "Installing SimpleHelp from Azure"
+# Check if SimpleHelp is already installed
+
+       
+# Define download URL
+$blobUrl = $SimpleHelpUrl
+
+# Define local download path
+$downloadPath = "C:\Temp\Remote-Access-windows64-online.exe"
+
+# Ensure C:\Temp exists
+if (-Not (Test-Path -Path "C:\Temp")) {
+    New-Item -Path "C:\Temp" -ItemType Directory | Out-Null
+}
+
+# Download the file
+Invoke-WebRequest -Uri $blobUrl -OutFile $downloadPath
+
+# Define installation arguments as an array
+$installArgs = @("/S", "/NAME=AUTODETECT", "/HOST=https://sh.stmonicatrust.org.uk:444", "/NOSHORTCUTS")
+
+# Run the installer
+Start-Process -FilePath $downloadPath -ArgumentList $installArgs -Wait -NoNewWindow
+
+
